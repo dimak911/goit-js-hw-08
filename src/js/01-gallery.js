7910ba6866248cf67a1b5f1d1577304b626e32e4
Dmytro Kniahnytskyi <dimak911@gmail.com>
2022-05-29 16:38:15 +0300

    hw-01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,37 @@
+// Описан в документации
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// Создание и рендер разметки по массиву данных galleryItems
+
+const galleryRef = document.querySelector('.gallery');
+
+insertMarkup(createGalleryMarkup(galleryItems));
+
+function createGalleryMarkup(arrayOfItems) {
+  return arrayOfItems
+    .map(({ description, original, preview }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" loading="lazy" alt="${description}" /></a>`;
+    })
+    .join('');
+}
+
+function insertMarkup(markupString) {
+  galleryRef.insertAdjacentHTML('afterbegin', markupString);
+}
+
+// Инициализация библиотеки simplelightbox
+const lightbox = new SimpleLightbox('.gallery a', {
+  /* options */
+  captions: true,
+  captionSelector: 'img',
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
